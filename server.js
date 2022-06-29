3511fb96ec01217ad5860af9412942a92996a9dd
mfkfawas <mfkfawas@gmail.com>
2022-06-29 12:54:55 +0530

    multer setup

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const dotenv = require('dotenv');
 dotenv.config();
 
+const multer = require('multer');
 const express = require('express');
 const app = express();
+//return a middleware
+const upload = multer({ dest: 'upload' });
 
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.post('/upload', (req, res) => {
+  res.send('hai buddies');
 });
 
 const server = app.listen(process.env.PORT, () => {
